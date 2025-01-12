7c82f0aa29171a2fe81b5684b63ea677be272075
nakshatramandowara <127446474+nakshatramandowara@users.noreply.github.com>
2025-01-13 03:27:10 +0530

    Update app2.py

--- Frontend/app2.py
+++ Frontend/app2.py
@@ -14,8 +14,11 @@ EVENTS_PATH = script_dir/"events.json"
 def start():
     er.initialize_collection("my_events")
 
-    with open(EVENTS_PATH, 'r') as f:
-        documents = json.load(f)
+    with open("events.json", "r", encoding="utf-8") as f:
+        raw_content = f.read()
+        print(raw_content)
+        documents = json.loads(raw_content)
+
 
     for idx, doc in enumerate(documents):
         doc["id"] = idx  
